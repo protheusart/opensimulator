b8b169421c3674627dbc9bd2b5f8e7d044e0d27c
protheusart <126279150+protheusart@users.noreply.github.com>
2023-03-09 05:25:12 +0100

    Update opensimulator.sh

--- opensimulator.sh
+++ opensimulator.sh
@@ -3,9 +3,6 @@
 # Actualizar el sistema
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y gnupg ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-jammy main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 sudo apt update
 sudo apt-get install -y mono-complete
 sudo apt-get install -y mysql-server
